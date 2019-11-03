9f63818d2392d76c24c5a78234c32e37b47c8b5a
Blax Fontaine <valerie.gehle@gmail.com>
2019-11-03 20:31:47 +0100

    devise setup and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :organizations do
     resources :nodes, only: [ :show, :new, :create, :edit, :update ]
